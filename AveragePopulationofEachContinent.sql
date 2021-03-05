fb3542fb06d7eb372e7b680678b6934c486d2f45
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-05 16:30:48 +0530

    Update AveragePopulationofEachContinent.sql

--- AveragePopulationofEachContinent.sql
+++ AveragePopulationofEachContinent.sql
@@ -1,6 +1,27 @@
 /*
+Try each query and move to result
+
+
 SELECT *
 FROM country cn
 JOIN city ct
 ON cn.code = ct.countrycode;
+
+SELECT cn.continent, ct.name, ct.population
+FROM country cn
+JOIN city ct
+ON cn.code = ct.countrycode;
+
+SELECT cn.continent, COUNT(ct.name)       -- counted cities to each continent
+FROM country cn
+JOIN city ct
+ON cn.code = ct.countrycode
+GROUP BY cn.continent;
 */
+SELECT 
+    cn.continent, 
+    FLOOR(AVG(ct.population))
+FROM country cn
+JOIN city ct
+    ON cn.code = ct.countrycode
+GROUP BY cn.continent;
